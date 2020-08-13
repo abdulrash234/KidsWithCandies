a6c164286754a8f101a7ebb5d2e001ace581faa9
Abdulrashid Mohammed <abmoh@dfds.com>
2020-08-13 10:42:55 +0200

    Add project files.

---
+++ KidsWithCandies/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KidsWithCandies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var candies = new int[] {2, 3, 5, 1, 3};
+            var list = KidsWithCandies(candies, 3);
+            foreach (var val in list)
+            {
+                Console.WriteLine(val);
+            }
+        }
+
+        public static IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+        {
+
+            var count = candies.Length;
+            var list = new List<bool>(count);
+            for (int i = 0; i < candies.Count(); i++)
+            {
+                var highestCandiesPossible = candies[i] + extraCandies;
+                var isGreatest = true;
+                for (int j = 0; j < count; j++)
+                {
+                    if (i == j)
+                    {
+                        continue;
+                    }
+                    if (highestCandiesPossible < candies[j])
+                    {
+                        isGreatest = false;
+                    }
+                }
+                list.Add(isGreatest);
+            }
+            return list;
+        }
+    }
+}
